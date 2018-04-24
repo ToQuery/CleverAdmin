d32d47a1b645fe88494f50366b8e653a8d0b429d
ToQuery <toquery@qq.com>
2018-04-24 15:00:53 +0800

    inti

---
+++ src/main/java/com/toquery/cleverweb/CleverWebApplication.java
@@ -0,0 +1,16 @@
+package com.toquery.cleverweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class CleverWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleverWebApplication.class, args);
+	}
+
+
+}

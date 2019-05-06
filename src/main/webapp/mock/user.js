5d1bdb7ee73af6d0ec70e08af4ea449092cc8cbc
ToQuery <toquery@qq.com>
2019-05-06 15:11:08 +0800

    Merge commit '09ba38edae3ec3776d295684cae67a220fcadd02' into include_subtree
    # Conflicts:
    #	src/main/webapp/src/lang/es.js
    #	src/main/webapp/src/router/index.js

--- src/main/webapp/mock/user.js
+++ src/main/webapp/mock/user.js
@@ -10,7 +10,7 @@ const tokens = {
 
 const users = {
   'admin-token': {
-    roles: ['admin'],
+    roles: ['admin', 'editor'],
     introduction: 'I am a super administrator',
     avatar: 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif',
     name: 'Super Admin'

19884e799388cfc0d9fe601cc427a4599f4105b0
ToQuery <toquery@qq.com>
2019-06-24 20:20:14 +0800

    增加ueditor 支持

--- src/main/webapp/src/views/components-demo/ueditor.vue
+++ src/main/webapp/src/views/components-demo/ueditor.vue
@@ -13,15 +13,13 @@
 
 <script>
 import VueUeditorWrap from 'vue-ueditor-wrap'
+process.env.VUE_APP_BASE_API
 export default {
   name: 'UeditorDemo',
   components: { VueUeditorWrap },
   data() {
     return {
-      content:
-      `<h1 style="text-align: center;">Welcome to the TinyMCE demo!</h1><p style="text-align: center; font-size: 15px;"><img title="TinyMCE Logo" src="//www.tinymce.com/images/glyph-tinymce@2x.png" alt="TinyMCE Logo" width="110" height="97" /><ul>
-        <li>Our <a href="//www.tinymce.com/docs/">documentation</a> is a great resource for learning how to configure TinyMCE.</li><li>Have a specific question? Visit the <a href="https://community.tinymce.com/forum/">Community Forum</a>.</li><li>We also offer enterprise grade support as part of <a href="https://tinymce.com/pricing">TinyMCE premium subscriptions</a>.</li>
-      </ul>`,
+      content: `<h1 style="text-align: center;">Welcome to the ueditor demo!</h1>`,
       ueditorConfig: {
         // 编辑器不自动被内容撑高
         autoHeightEnabled: false,
@@ -30,7 +28,7 @@ export default {
         // 初始容器宽度
         initialFrameWidth: '100%',
         // 上传文件接口（这个地址是我为了方便各位体验文件上传功能搭建的临时接口，请勿在生产环境使用！！！）
-        serverUrl: '/app/files/ueditor',
+        serverUrl: '/app/ueditor',
         // UEditor 资源文件的存放路径，如果你使用的是 vue-cli 生成的项目，通常不需要设置该选项，vue-ueditor-wrap 会自动处理常见的情况，如果需要特殊配置，参考下方的常见问题2
         UEDITOR_HOME_URL: '/static/UEditor/'
       }

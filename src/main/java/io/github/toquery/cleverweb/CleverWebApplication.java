247ce035f9d08075bb8cfcc5e48c9a3bedb6424e
ToQuery <toquery@qq.com>
2019-12-04 19:08:24 +0800

    修改bean加载方式，删除富文本编辑器

--- src/main/java/io/github/toquery/cleverweb/CleverWebApplication.java
+++ src/main/java/io/github/toquery/cleverweb/CleverWebApplication.java
@@ -1,20 +1,21 @@
 package io.github.toquery.cleverweb;
 
-import io.github.toquery.framework.dao.EnableAppJpaRepositories;
-import io.github.toquery.framework.data.rest.annotation.EnableAppRepositoryRest;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 // import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
-@EnableAppRepositoryRest
+//(scanBasePackages = {"io.github.toquery.framework.system", "io.github.toquery.framework.security"})
+//@EnableAppRepositoryRest
 //@EntityScan(basePackages = {"io.github.toquery.framework.security.domain"})
-@EnableAppJpaRepositories // (basePackages = {"io.github.toquery.framework.security"})
+//@EnableAppJpaRepositories(basePackages = {"io.github.toquery.framework.system", "io.github.toquery.framework.security"})
 public class CleverWebApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(CleverWebApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    new SpringApplicationBuilder(CleverWebApplication.class)
+      //.beanNameGenerator(new PackageBeanNameGenerator())
+      .run(args);
+    // SpringApplication.run(CleverWebApplication.class, args);
+  }
 }
 
---
+++ src/main/java/io/github/toquery/cleverweb/PackageBeanNameGenerator.java
@@ -0,0 +1,32 @@
+package io.github.toquery.cleverweb;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.context.annotation.AnnotationBeanNameGenerator;
+
+import java.beans.Introspector;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+@Slf4j
+public class PackageBeanNameGenerator extends AnnotationBeanNameGenerator {
+
+  @Override
+  public String generateBeanName(BeanDefinition definition, BeanDefinitionRegistry registry) {
+    String beanClassName = Introspector.decapitalize(definition.getBeanClassName());
+    if (StringUtils.startsWith(beanClassName, getPackageNamePrefix())) {
+      log.info("初始化Bean: " + beanClassName);
+      return beanClassName;
+    }
+    return super.generateBeanName(definition, registry);
+  }
+
+  private String getPackageNamePrefix() {
+    String separator = ".";
+    String[] splits = StringUtils.split(this.getClass().getPackage().getName(), separator);
+    String prefix = Arrays.stream(splits).limit(3).collect(Collectors.joining(separator));
+    return prefix;
+  }
+}
